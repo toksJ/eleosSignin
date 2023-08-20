bfb0443a948bac69b414f9b002678b4b7cd0f393
toksJ <tokajaber11@gmail.com>
2023-08-21 03:13:51 +0400

    Initial Commit

---
+++ eleosSignin/eleosSigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  eleosSigninApp.swift
+//  eleosSignin
+//
+//  Created by TokaJaber on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct eleosSigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
